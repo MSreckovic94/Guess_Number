1f5bf20817d9d9899fc054127863bc9c5fe0cabb
MSreckovic94 <84035115+MSreckovic94@users.noreply.github.com>
2021-05-19 16:50:32 +0200

    bug?

--- script.js
+++ script.js
@@ -35,11 +35,11 @@ const testWin = function () {
   document.querySelector('body').style.backgroundColor = '#60b347';
   document.querySelector('.number').style.width = '30rem';
   document.querySelector('.number').textContent = secretNumber;
-
-  if (score > finalScore) {
-    finalScore = score;
-    document.querySelector('.highscore').textContent = finalScore;
-  }
+  document.querySelector('.highscore').textContent = score;
+  // if (score > finalScore) {
+  //   finalScore = score;
+  //   document.querySelector('.highscore').textContent = finalScore;
+  // }
 };
 
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
